a2f7631fc8918edced4b12b3865b87707fe5aaf9
Divyanshu-hash <rishugiri056@gmail.com>
2026-01-14 00:51:43 +0530

    Removed unused media and stego modules

--- backend/app/stego/audio_image.py
+++
@@ -1,4 +0,0 @@
-def encode_audio(image, audio, output):
-    with open(audio, "rb") as f:
-        data = f.read()
-    encode_text(image, data.decode(errors="ignore"), output)
--- backend/app/stego/image_image.py
+++
@@ -1,4 +0,0 @@
-def encode_image(carrier, hidden, output):
-    with open(hidden, "rb") as f:
-        data = f.read()
-    encode_text(carrier, data.decode(errors="ignore"), output)
